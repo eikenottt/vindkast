d509355974a0498e1ff984fa7cf803cde62cfef3
Rune Eikemo <rune.eikem@gmail.com>
2017-05-02 14:20:05 +0200

    + endra fra hex color til rgba på header knapp (advanced search)

--- INFO134_Prosjekt/script/show_movie.js
+++ INFO134_Prosjekt/script/show_movie.js
@@ -8,15 +8,15 @@ function add_row(table, left, right) {
     left_cell = document.createElement("TD");
     left_cell.appendChild(left);
     new_row.appendChild(left_cell);
-    
+
     right_cell = document.createElement("TD");
     right_cell.appendChild(right);
     new_row.appendChild(right_cell);
-    
+
     table.appendChild(new_row);
 }
 
-window.onload = function() {
+window.onload = function () {
     query_params = get_query_string_parameters();
     if (!query_params.id) {
         panic("No id given");
@@ -24,23 +24,23 @@ window.onload = function() {
     }
 
     var xhr = new XMLHttpRequest();
-    
+
     xhr.open("GET", "http://wildboy.uib.no/mongodb/objects/?filter_id=" + query_params.id, true);
-    
+
     xhr.responseType = "json";
 
-    xhr.onload = function(){
-	movie_object = xhr.response.rows[0];
-	//console.log("Response", movie_object);
+    xhr.onload = function () {
+        movie_object = xhr.response.rows[0];
+        //console.log("Response", movie_object);
 
-	// add a "debug-table" on the bottom showing all elements from movie_object
-	stats_table = document.getElementById("movie_stat_table");
-	for (key in movie_object) {
+        // add a "debug-table" on the bottom showing all elements from movie_object
+        stats_table = document.getElementById("movie_stat_table");
+        for (key in movie_object) {
             left = document.createTextNode(key);
             right = document.createTextNode(movie_object[key]);
             add_row(stats_table, left, right);
-	}
-	
+        }
+
     };
-    xhr.send();    
+    xhr.send();
 };
